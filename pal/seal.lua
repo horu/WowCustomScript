aa32c5746b230833ef45e7d60a75ad6cc03a8ca9
Andrey <andrey.s.novosib@gmail.com>
2023-04-22 07:12:46 +0600

    pal.seal format

--- pal/seal.lua
+++ pal/seal.lua
@@ -1,9 +1,6 @@
-
 local cs = cs_common
 local pal = cs.pal
 local cast = pal.cast
-
-
 
 local seal_Righteousness = "Seal of Righteousness"
 local seal_Crusader = "Seal of the Crusader"
@@ -12,19 +9,18 @@ local seal_Light = "Seal of Light"
 local seal_Wisdom = "Seal of Wisdom"
 local seal_list_all = { seal_Righteousness, seal_Crusader, seal_Justice, seal_Light, seal_Wisdom }
 
-
 local to_short_list = {}
-to_short_list[seal_Righteousness] = cs.color_purple.."SR".."|r"
-to_short_list[seal_Crusader] = cs.color_orange_1.."SC".."|r"
-to_short_list[seal_Light] = cs.color_yellow.."SL".."|r"
-to_short_list[seal_Justice] = cs.color_green.."SJ".."|r"
-to_short_list[seal_Wisdom] = cs.color_blue.."SW".."|r"
+to_short_list[seal_Righteousness] = cs.color_purple .. "SR" .. "|r"
+to_short_list[seal_Crusader] = cs.color_orange_1 .. "SC" .. "|r"
+to_short_list[seal_Light] = cs.color_yellow .. "SL" .. "|r"
+to_short_list[seal_Justice] = cs.color_green .. "SJ" .. "|r"
+to_short_list[seal_Wisdom] = cs.color_blue .. "SW" .. "|r"
 
 local to_short = function(spell_name)
-    if not spell_name then
-        return cs.color_grey.."XX".."|r"
-    end
-    return to_short_list[spell_name]
+  if not spell_name then
+    return cs.color_grey .. "XX" .. "|r"
+  end
+  return to_short_list[spell_name]
 end
 
 
@@ -35,82 +31,81 @@ end
 pal.Seal = cs.create_class()
 
 pal.Seal.build = function(buff, target_debuff, target_hp_limit)
-    local seal = pal.Seal:new()
+  local seal = pal.Seal:new()
 
-    seal.buff = cs.Buff.build(buff)
-    seal.target_debuff = target_debuff
-    seal.judgement = cs.Spell.build(cast.Judgement)
-    seal.target_hp_limit = target_hp_limit or 0
+  seal.buff = cs.Buff.build(buff)
+  seal.target_debuff = target_debuff
+  seal.judgement = cs.Spell.build(cast.Judgement)
+  seal.target_hp_limit = target_hp_limit or 0
 
-    cs.debug(seal)
+  cs.debug(seal)
 
-    return seal
+  return seal
 end
 
 pal.Seal.current_to_string = function()
-    local seal_name = cs.find_buff(seal_list_all)
-    return to_short(seal_name)
+  local seal_name = cs.find_buff(seal_list_all)
+  return to_short(seal_name)
 end
 
 
 -- const
 function pal.Seal:is_judgement_available()
-    if self:check_target_debuff() then
-        return
-    end
+  if self:check_target_debuff() then
+    return
+  end
 
-    return self:is_reseal_available()
+  return self:is_reseal_available()
 end
 
 function pal.Seal:check_target_debuff()
-    if not self.target_debuff then
-        return
-    end
+  if not self.target_debuff then
+    return
+  end
 
-    return cs.has_debuffs(cs.u_target, self.target_debuff)
+  return cs.has_debuffs(cs.u_target, self.target_debuff)
 end
 
 -- const
 function pal.Seal:check_exists()
-    return self.buff:check_exists()
+  return self.buff:check_exists()
 end
 
 -- seal can be casted
 -- const
 function pal.Seal:is_reseal_available()
-    if not cs.check_target(cs.t_attackable) then
-        return
-    end
+  if not cs.check_target(cs.t_attackable) then
+    return
+  end
 
-    local target_hp = UnitHealth(cs.u_target) or 0
-    return target_hp >= self.target_hp_limit
+  local target_hp = UnitHealth(cs.u_target) or 0
+  return target_hp >= self.target_hp_limit
 end
 
 function pal.Seal:reseal()
-    if not self:is_reseal_available() then
-        return cs.Buff.failed
-    end
-    return self.buff:rebuff()
+  if not self:is_reseal_available() then
+    return cs.Buff.failed
+  end
+  return self.buff:rebuff()
 end
 
 -- return true on success cast
 function pal.Seal:reseal_and_cast(...)
-    if self:reseal() then
-        return
-    end
+  if self:reseal() then
+    return
+  end
 
-    local order = cs.SpellOrder.build(unpack(arg))
-    return order:cast()
+  local order = cs.SpellOrder.build(unpack(arg))
+  return order:cast()
 end
 
 -- check the seal exists and the target has no the seal debuff
 function pal.Seal:judgement_it()
-    if self:check_exists() and self:is_judgement_available() then
-        self.judgement:cast()
-        return true
-    end
+  if self:check_exists() and self:is_judgement_available() then
+    self.judgement:cast()
+    return true
+  end
 end
-
 
 pal.seal = {}
 cs.once_event(0.2, function()
@@ -126,6 +121,6 @@ cs.once_event(0.2, function()
           "Spell_Holy_RighteousnessAura",
           UnitHealthMax(cs.u_player) * 0.2
   )
-  pal.seal.Justice = pal.Seal.build(seal_Justice,"Spell_Holy_SealOfWrath")
+  pal.seal.Justice = pal.Seal.build(seal_Justice, "Spell_Holy_SealOfWrath")
 end)
 
